01d33f31cbb0970b0ea0492e7b75d2142ae59809
Ansar <ansarshayekin1@gmail.com>
2025-05-21 16:23:46 +0100

    modifying images and its container

--- ImageSlider.js
+++ ImageSlider.js
@@ -6,12 +6,14 @@ class ImageSlider {
     slidesWrapper;
     slides;
     imagesContainer = [];
+    imagesCount;
+    slideByCount;
 
-    constructor(element, imagesCount, mainImageId, slideByCount) {
+    constructor(element = document.body, imagesCount = 3, mainImageId = 1, slideByCount = 1) {
         this.element = element;
         this.createSlider();
-        // imagesCount = this.slides.style.width = this.distanceImages() * imagesCount;
-        this.slides.style.width = this.distanceImages() * imagesCount + "px";
+        this.imagesCount = imagesCount;
+        this.slideByCount = slideByCount;
     }
 
     createSlider() {
@@ -24,8 +26,16 @@ class ImageSlider {
         const arrowLeft = document.createElement("button");
         arrowLeft.classList.add("arrow", "arrow-left");
 
+        arrowLeft.addEventListener("click", () => {
+            this.goBack();
+        });
+
         const arrowRight = document.createElement("button");
         arrowRight.classList.add("arrow", "arrow-right");
+
+        arrowRight.addEventListener("click", () => {
+            this.goForward();
+        });
 
         this.slides = document.createElement("div");
         this.slides.classList.add("slides");
@@ -45,34 +55,62 @@ class ImageSlider {
     }
 
     loadImages(imageArray = []) {
-        for (let i = 0; i < imageArray.length; i++) {
+        for (let i = 0; i < imageArray.length + (this.slideByCount * 2); i++) {
             this.slidesImages = document.createElement("div");
             this.slidesImages.classList.add("slides-images");
-            this.slidesWrapper.append(this.slidesImages);
 
             const images = document.createElement("img");
-            images.src = imageArray[i];
+            images.src = imageArray[i % imageArray.length];
 
             this.slidesImages.append(images);
-            this.imagesContainer.push(images);
-            // console.log(this.imagesContainer);
+            this.imagesContainer.push(this.slidesImages);
+            // this.slidesImages.style.transform = "translateX(-115px)";
+            // this.slidesImages.style.transform = `translateX(${-this.getImagesDistance()}px)`;
+
+            this.slidesWrapper.append(this.slidesImages);
         }
+        this.transformImages();
+        this.resizeContainer();
     }
 
 
-    distanceImages() {
-        const distanceX = this.imagesContainer[1].getBoundingClientRect().x - this.imagesContainer[0].getBoundingClientRect().x;
+    transformImages() {
+        const imagesDistance = this.getImagesDistance();
+        for (let i = 0; i < this.imagesContainer.length; i++) {
+            // this.slidesWrapper.firstElementChild.before(this.slidesWrapper.lastElementChild);
+            this.imagesContainer[i].style.transform = `translate(${-imagesDistance}px`;
+            // this.slidesImages.style.transform = `translateX(${-imagesDistance}px)`;
+        }
+        console.log(this.slidesWrapper.firstElementChild);
+        console.log(this.slidesWrapper.lastElementChild);
+    }
 
-        // const slides = document.getElementsByClassName("slides")[0].style.width = distanceX + "px";
-        // console.log(distanceX);
+    // 1-ый цикл генерирует контейнер с изоброжениями
+    // 2-ой цикл генерирует 2 контейнера с изоброжениями ссылаясь на исходный размер род контейнера (imageArray.length + + (slideByCount * 2))
+
+    getImagesDistance() {
+        let distanceX = 0;
+        if (this.imagesContainer.length > 1) {
+            distanceX = this.imagesContainer[1].getBoundingClientRect().x - this.imagesContainer[0].getBoundingClientRect().x;
+        } else {
+            distanceX = this.imagesContainer[0].getBoundingClientRect().x;
+        }
         return distanceX;
     }
 
-    goForward() {
+    resizeContainer() {
+        this.slides.style.width = this.getImagesDistance() * this.imagesCount + "px";
+    }
 
+
+    goForward() {
+        this.slidesWrapper.style.transform = `translateX(${this.getImagesDistance()}px)`;
+        // this.imagesContainer[0].before(this.imagesContainer[this.imagesContainer.length - 1]);
+        // console.log(this.imagesContainer);
     }
 
     goBack() {
+        this.slidesWrapper.style.transform = `translateX(${-this.getImagesDistance()}px)`;
 
     }
 }
